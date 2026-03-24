6f133b0e0915ea6aa9dd294878fc9ef8af0fae30
Farhane Lawani <2489702@cegepmontpetit.ca>
2026-03-24 19:14:08 -0400

    Fct angular

--- question2/ngSignalR/src/app/app.component.ts
+++ question2/ngSignalR/src/app/app.component.ts
@@ -27,26 +27,70 @@ export class AppComponent {
     this.connect();
   }
 
-  connect() {
+  async connect() {
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl('http://localhost:5282/hubs/pizza')
       .build();
 
+    //Met à jour le nombre d’utilisateurs connectés.
+    this.hubConnection.on('UpdateNbUsers', (nbUsers: number) => {
+      this.nbUsers = nbUsers;
+    });
+
+    //Met à jour l’argent du groupe.
+    this.hubConnection.on('UpdateMoney', (money: number) => {
+      this.money = money;
+    });
+
+    //Met à jour le nombre de pizzas et l’argent.
+    this.hubConnection.on('UpdateNbPizzasAndMoney', (nbPizzas: number, money: number) => {
+      this.nbPizzas = nbPizzas;
+      this.money = money;
+    });
+
+    //Met à jour le prix de la pizza sélectionnée.
+    this.hubConnection.on('UpdatePizzaPrice', (pizzaPrice: number) => {
+      this.pizzaPrice = pizzaPrice;
+    });
+    
     // TODO: Mettre isConnected à true seulement une fois que la connection au Hub est faite
-    this.isConnected = true;
+    await this.hubConnection.start(); //Connection
+    this.isConnected = true; 
   }
 
-  selectChoice(selectedChoice:number) {
+  //choisit la nouvelle pizza et appelle la méthode du Hub
+  async selectChoice(selectedChoice:number) {
+    if (this.selectedChoice != -1) {
+      await this.hubConnection?.invoke('UnselectChoice', this.selectedChoice);
+    }
+
     this.selectedChoice = selectedChoice;
+    await this.hubConnection?.invoke('SelectChoice', this.selectedChoice);
   }
 
-  unselectChoice() {
+  //enlève l’utilisateur de l’ancien groupe
+  async unselectChoice() {
+    if (this.selectedChoice != -1) {
+      await this.hubConnection?.invoke('UnselectChoice', this.selectedChoice);
+    }
+
     this.selectedChoice = -1;
+    this.pizzaPrice = 0;
+    this.money = 0;
+    this.nbPizzas = 0;
   }
 
-  addMoney() {
+  //elle appelle AddMoney sur le Hub si il y a une pizza selectionné
+  async addMoney() {
+    if (this.selectedChoice != -1) {
+      await this.hubConnection?.invoke('AddMoney', this.selectedChoice);
+    }
   }
 
-  buyPizza() {
+  //elle appelle BuyPizza sur le Hub
+  async buyPizza() {
+    if (this.selectedChoice != -1) {
+      await this.hubConnection?.invoke('BuyPizza', this.selectedChoice);
+    }
   }
 }
